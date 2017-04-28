1d1d0e67d452f0e3e226aef04fde3c62e367f43b
shavit <chernihov>
2017-04-28 14:07:05 +0300

    Revert "d"
    This reverts commit fe7905ffb3a98a93437552cabff58dd62aa0cf82.

---
+++ TexasHoldem/illegalChipPolicyException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TexasHoldem
+{
+    [Serializable]
+    internal class illegalChipPolicyException : Exception
+    {
+        public illegalChipPolicyException()
+        : base() { }
+
+        public illegalChipPolicyException(string message)
+        : base(String.Format("Invalid chip policy: {0} , expected not negative value." , message)) { }
+
+    }
+}
