6f487aceab6d50cac139c58a10dcb66105cdb57f
shavit <chernihov>
2017-04-28 13:34:14 +0300

    Revert "add lock for future thread to createGame"
    This reverts commit eb28ff388811babc3e455200e04fd46711bbcdc6.

--- TexasHoldem/GameCenter.cs
+++ TexasHoldem/GameCenter.cs
@@ -12,7 +12,6 @@ namespace TexasHoldem
     {
         private Dictionary<int, Game> games;   // int-Game id
         private DataBase db;
-        private System.Object lockThis = new System.Object();
 
         GameCenter()
         {
@@ -40,31 +39,28 @@ namespace TexasHoldem
         }
         public Game CreateGame(User user, int gameType, int buyIn, int chipPolicy, int minBet, int maxPlayers, int minPlayers, bool spectateGame)
         {
-            lock (lockThis)
-            {
-                if (gameType < 0 || gameType > 2)
-                    throw new illegalGameTypeException(gameType.ToString());
-                if (buyIn < 0)                                 //real money and has to be equal or greater than zero
-                    throw new illegalbuyInException(buyIn.ToString());
-                if (chipPolicy < 0)
-                    throw new illegalChipPolicyException(chipPolicy.ToString());
-                if (minBet <= 0)
-                    throw new illegalMinBetException(minBet.ToString());
-                if (minPlayers < 2)
-                    throw new illegalMinPlayersException(minPlayers.ToString());
-                if (minPlayers > maxPlayers)
-                    throw new illegalGapPlayersException(minPlayers.ToString(), maxPlayers.ToString());
-                if (maxPlayers > 9)
-                    throw new illegalMaxPlayersException(maxPlayers.ToString());
-                if (buyIn > 0 && user.getMoney < buyIn)
-                    throw new notEnoughMoneyException(user.getMoney, buyIn);
-                GamePreferences pref = new GamePreferences(gameType, buyIn, chipPolicy, minBet, maxPlayers, minPlayers, spectateGame);
-                Game game = new Game(pref);
-                game.addPlayer(user);
-                games.Add(game.Id, game);
-                db.AddGame(game);
-                return game;
-            }
+            if (gameType < 0 || gameType > 2)
+                throw new illegalGameTypeException(gameType.ToString());
+            if(buyIn<0)                                 //real money and has to be equal or greater than zero
+                throw new illegalbuyInException(buyIn.ToString());
+            if(chipPolicy < 0)
+                throw new illegalChipPolicyException(chipPolicy.ToString());
+            if (minBet <= 0)
+                throw new illegalMinBetException(minBet.ToString());
+            if(minPlayers < 2)
+                throw new illegalMinPlayersException(minPlayers.ToString());
+            if (minPlayers > maxPlayers)
+                throw new illegalGapPlayersException(minPlayers.ToString(), maxPlayers.ToString());
+            if(maxPlayers>9)
+                throw new illegalMaxPlayersException(maxPlayers.ToString());
+            if (buyIn > 0 && user.getMoney < buyIn)
+                throw new notEnoughMoneyException(user.getMoney,buyIn);
+            GamePreferences pref = new GamePreferences(gameType, buyIn, chipPolicy, minBet, maxPlayers, minPlayers, spectateGame);
+            Game game = new Game(pref);
+            game.addPlayer(user);
+            games.Add(game.Id, game);
+            db.AddGame(game);
+            return game;
         }
         public bool SetDefaultLeague(League league)
         {
