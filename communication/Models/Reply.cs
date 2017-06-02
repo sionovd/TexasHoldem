4c84ee76d2c0ecb6458b86cba2804a4b28c64d97
avnerrhh <huriavne@post.bgu.ac.il>
2017-06-02 11:17:12 +0300

    try2

---
+++ communication/Models/Reply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace communication.Models
+{
+    public class Reply
+    {
+        [JsonProperty("StringContext")]
+        public string StringContext { get; set; }
+        [JsonProperty("IntContext")]
+        public int IntContext { get; set; }
+        [JsonProperty("Sucsses")]
+        public bool Sucsses { get; set; }
+        [JsonProperty("ExtraData")]
+        public List<int> ExtraData { get; set; }
+
+        public Reply(string context, bool sucsess, List<int> data, int intContent)
+        {
+            Sucsses = sucsess;
+            StringContext = context;
+            ExtraData = data;
+            IntContext = intContent;
+
+        }
+    }
+}
